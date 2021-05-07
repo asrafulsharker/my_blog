2a6667e6ed0aa33c8f04214b35cb3e129e0f73b5
Md Asraful Sharker  Nirob <64266026+asrafulsharker@users.noreply.github.com>
2021-05-07 15:32:32 +0600

    git add

--- App_Blog/models.py
+++ App_Blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Blog(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_author')
+    blog_title = models.CharField(max_length=264, verbose_name="Put a Title")
--- my_blog/settings.py
+++ my_blog/settings.py
@@ -9,14 +9,14 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
-from pathlib import Path
 
+from pathlib import Path
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.pathjoin(BASE_DIR, 'templates')
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR =os.path.join(BASE_DIR, 'static')
-MEDIA_DIR = os.path(BASE_DIR, 'media')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
